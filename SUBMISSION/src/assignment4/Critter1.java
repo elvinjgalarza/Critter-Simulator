cf8b2f9f0823d186bd3990c69c216635e84435b9
Elvin <elvinjohngalarza@gmail.com>
2018-03-22 22:35:46 -0500

    added the submission folder

---
+++ SUBMISSION/src/assignment4/Critter1.java
@@ -0,0 +1,48 @@
+package assignment4;
+
+import assignment4.Critter.TestCritter;
+
+/* The Morty Critter's life has always been down.
+   It only knows being down, and so it moves downward.
+   It spits out babies downwards. It always runs away
+   from fights; however, when Morty is on the same spot as
+   a Rick Critter, Morty becomes enraged and fights him.
+ */
+
+
+public class Critter1 extends TestCritter {
+
+    /**
+     * String representation of the Critter to display on board
+     * @return returns the string representation of Morty
+     */
+    @Override
+    public String toString () { return "M"; }
+
+    /**
+     * Does the timestep for this Critter
+     */
+    @Override
+    public void doTimeStep() {
+        walk(6);
+        run(6);
+        if (getEnergy() >= Params.min_reproduce_energy) {
+            Critter1 child = new Critter1();
+            reproduce(child, 6);
+        }
+    }
+
+    /**
+     * Tells if should fight or not
+     * @param opponent Opponent Critter
+     * @return boolean that says to fight/not to fight
+     */
+    @Override
+    public boolean fight(String opponent) {
+        if (opponent.equals("R")) {
+            return true;
+        }
+        return false;
+    }
+
+}
